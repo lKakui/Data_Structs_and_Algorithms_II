d403b70cc0297fc37d5d2c4ca022d557d80d272f
Luiz P. Kakuichi <luizpkakuichi@gmail.com>
2024-12-06 17:20:44 -0300

    include hash.h

---
+++ Tde_2/include/hash.h
@@ -0,0 +1,22 @@
+#ifndef HASH_H
+#define HASH_H
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stddef.h> // Para garantir acesso a NULL
+
+#include "../include/reader.h"
+
+#define B 256  // Número de buckets hash
+
+// Estrutura para representar uma hash
+typedef struct hash {
+    int adress;
+    int value;
+    int key; // Chave para ordenação
+    struct hash *next; // Ponteiro para lidar com colisões (encadeamento)
+} HASH;
+
+int create_hash_index(FILE *input, size_t model);
+
+#endif // HASH.H
